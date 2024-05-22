1fd01959349dc180ce7bef47aeab90922ffdece9
Venki-karra <56701415+Venki-karra@users.noreply.github.com>
2024-05-22 10:40:36 -0500

    Added new project given my Cornell team

---
+++ MAX14662_gaincontrol/build/startup_max32665.d
@@ -0,0 +1,2 @@
+C:/Users/Vkarra/Documents/GitHub/Swift_DSS/SWIFTV2_DSS/MAX14662_gaincontrol/build/startup_max32665.o: \
+ C:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX32665/Source/GCC/startup_max32665.S
